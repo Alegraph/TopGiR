5f97845d59bf1e06a1d9c6090a9d0154e0e78504
Cozar <olegtch8@mail.ru>
2022-05-17 20:08:40 +0300

    creating project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    /*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*/
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
